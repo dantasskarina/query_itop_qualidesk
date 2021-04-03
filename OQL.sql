cba178c757875f22543c7409e5fc178466c6766a
Karina Dantas Luiz <karinadantasluiz@gmail.com>
2021-04-03 20:10:38 -0300

    versao 1.1

--- OQL.sql
+++ OQL.sql
@@ -1,7 +1,9 @@
 --chamados requisições para a equipe da accenture.
 SELECT ur  FROM UserRequest AS ur WHERE ur.status NOT IN ("closed","resolved") AND ur.team_id = 1997
+
 --chamados incidentes para a equipe da accenture.
 SELECT i FROM Incident AS i WHERE i.status NOT IN ("closed","resolved") AND i.team_id = 1997
+
 --chamados abertos maiores que um 15 dias.
 SELECT UserRequest
 WHERE
